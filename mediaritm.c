ac59842a17a1f7373206d5b7a812cfb427be883a
Nicola-Controne <109146662+Nicola-Controne@users.noreply.github.com>
2022-07-14 03:55:49 -0400

    Add files via upload

--- mediaritm.c
+++ mediaritm.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-	int num1, num2;	/*Dichiarazione variabili*/
+	float num1, num2;	/*Dichiarazione variabili*/
 	float media;
 
 	printf("\nInserire il primo numero intero: ");	/*Richiesta all'utente*/
-	scanf("%d", &num1);
+	scanf("%f", &num1);
 	printf("\n\nInserire il secondo numero intero: ");
-	scanf("%d", &num2);
+	scanf("%f", &num2);
 
 	media=(num1+num2)/2;	/*Media aritmetica*/
 
-	printf("\n\nLa media tra i due numeri e\': %f ", media);	/*Stampa del risultato*/
+	printf("\n\nLa media tra i due numeri e\': %5.1f ", media);	/*Stampa del risultato*/
 
 	return 0;
 }
