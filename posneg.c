eb795996f945a300772634655dfcaaf49e7f05a0
Nicola-Controne <109146662+Nicola-Controne@users.noreply.github.com>
2022-07-14 04:06:15 -0400

    Add files via upload

---
+++ posneg.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;	/*Dichiarazione variabile*/
+
+	printf("\nInserire un numero intero: ");	/*Richiesta all'utente*/
+	scanf("%d", &num);
+
+	if(num>0)	/*Varie condizioni a seconda dell'input inserito con stampa inclusa*/
+	{
+		printf("\n\nIl numero inserito e\' positivo");
+	}
+	if(num<0)
+	{
+		printf("\n\nIl numero inserito e\' negativo");
+	}
+	if(num==0)
+	{
+		printf("\n\nIl numero inserito e\' neutro");
+	}
+
+	return 0;	/*Fine programma*/
+}
